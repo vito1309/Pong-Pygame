9a68848bbb23b19f0ef8d8dd8fcaab52131dc041
vito1309 <vitornegri2005@gmail.com>
2026-03-24 20:43:04 -0300

    feat: adiciona rebote variavel com angulo aleatorio

--- ball.py
+++ ball.py
@@ -1,8 +1,12 @@
+import random
 import pygame
 from settings import LARGURA, ALTURA, BOLA_RAIO, BOLA_VELOCIDADE, BRANCO
 
+VARIACAO_MAXIMA = 2
+
 
 class Bola:
+
     def __init__(self):
         self.raio = BOLA_RAIO
         self.resetar(direcao=1)
@@ -17,19 +21,31 @@ class Bola:
     def rect(self):
         return pygame.Rect(self.x - self.raio, self.y - self.raio, self.raio * 2, self.raio * 2)
 
+    def _aplicar_variacao(self):
+        self.vel_y += random.uniform(-VARIACAO_MAXIMA, VARIACAO_MAXIMA)
+        self.vel_y = max(-8, min(8, self.vel_y))
+
     def atualizar(self):
         self.x += self.vel_x
         self.y += self.vel_y
 
         if self.y - self.raio <= 0 or self.y + self.raio >= ALTURA:
             self.vel_y = -self.vel_y
+            self._aplicar_variacao()
+            return True
+        return False
 
     def rebater_raquete(self, raquete_rect):
         if self.rect.colliderect(raquete_rect):
             if raquete_rect.centerx < LARGURA // 2 and self.vel_x < 0:
                 self.vel_x = -self.vel_x
+                self._aplicar_variacao()
+                return True
             elif raquete_rect.centerx > LARGURA // 2 and self.vel_x > 0:
                 self.vel_x = -self.vel_x
+                self._aplicar_variacao()
+                return True
+        return False
 
     def saiu_pela_esquerda(self):
         return self.x + self.raio < 0

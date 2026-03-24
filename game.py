be02cee1a60f9dc105ed39b8d2685792f62626e9
vito1309 <vitornegri2005@gmail.com>
2026-03-24 20:46:53 -0300

    feat: adiciona power-up de fragmentacao de bolas

--- game.py
+++ game.py
@@ -6,9 +6,12 @@ from paddle import Raquete
 from scoreboard import Placar
 from ai_controller import ControladorIA
 from renderer import Renderer
+from audio_manager import AudioManager
+from powerup import GerenciadorPowerUp
 
 
 class Jogo:
+
     def __init__(self):
         pygame.init()
         self.tela = pygame.display.set_mode((LARGURA, ALTURA))
@@ -16,12 +19,14 @@ class Jogo:
         self.clock = pygame.time.Clock()
         self.renderer = Renderer(self.tela)
         self.ia = ControladorIA()
+        self.audio = AudioManager()
 
     def _criar_entidades(self):
         self.bola = Bola()
         self.raquete1 = Raquete(x=MARGEM_RAQUETE)
         self.raquete2 = Raquete(x=LARGURA - MARGEM_RAQUETE - RAQUETE_LARGURA)
         self.placar = Placar()
+        self.powerup = GerenciadorPowerUp()
 
     def _processar_eventos(self):
         for evento in pygame.event.get():
@@ -54,23 +59,33 @@ class Jogo:
             self._processar_eventos()
             self._mover_jogador()
             self.ia.atualizar(self.raquete2, self.bola)
-            self.bola.atualizar()
-            self.bola.rebater_raquete(self.raquete1.rect)
-            self.bola.rebater_raquete(self.raquete2.rect)
+
+            bateu_parede = self.bola.atualizar()
+            if bateu_parede:
+                self.audio.tocar_parede()
+
+            if self.bola.rebater_raquete(self.raquete1.rect):
+                self.audio.tocar_raquete()
+            if self.bola.rebater_raquete(self.raquete2.rect):
+                self.audio.tocar_raquete()
 
             if self.bola.saiu_pela_esquerda():
                 self.placar.ponto_jogador2()
+                self.audio.tocar_ponto()
                 self.bola.resetar(direcao=1)
 
             if self.bola.saiu_pela_direita():
                 self.placar.ponto_jogador1()
+                self.audio.tocar_ponto()
                 self.bola.resetar(direcao=-1)
+
+            self.powerup.atualizar(self.bola, self.raquete1, self.raquete2)
 
             vencedor = self.placar.vencedor()
             if vencedor:
                 return vencedor
 
-            self.renderer.desenhar_jogo(self.bola, self.raquete1, self.raquete2, self.placar)
+            self.renderer.desenhar_jogo(self.bola, self.raquete1, self.raquete2, self.placar, self.powerup)
             pygame.display.flip()
             self.clock.tick(FPS)
 
